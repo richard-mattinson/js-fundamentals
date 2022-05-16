f5b83ee4c39a3fcdd42c5c5d7789e1e9715e91ae
Richard Mattinson <richard.m@ttinson.net>
2022-05-16 14:27:15 +0100

    declaration assignments completed

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -3,6 +3,9 @@
 //
 // TODO: Declare the variables firstName and age so that the tests pass
 
+const firstName = 'Jane'
+const age = 35
+
 // do not edit below this line
 let firstNameExport = ''
 try { firstNameExport = firstName } catch (e) {}
