617b8273970bc0312b50b29aff00a74faddad08b
Richard Mattinson <richard.m@ttinson.net>
2022-05-17 14:08:05 +0100

    completed for loops basic

--- src/loops/for-loop-basic.js
+++ src/loops/for-loop-basic.js
@@ -4,13 +4,13 @@ const evenNums = []
 const countdown = []
 
 // TODO: Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
+for (let i = 0; i <4; i++) {numsZeroToThree[i] = i;}
 // TODO: Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-
+for (let i = 5; i <= 10; i++) {numsFiveToTen.push (i)}
 // TODO: Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
-
+for (let i = 0; i <= 6; i+=2) {evenNums.push(i)}
 // TODO: Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
+for (let i = 3; i >= 0; i=i-1) {countdown.push(i)}
 // do not change below this line
 module.exports = {
   a: numsZeroToThree,
