8c83d8290731028b3bf93b34e361dc6985e472bc
Richard Mattinson <richard.m@ttinson.net>
2022-09-20 19:44:02 +0100

    multiple conditionals complete

--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -1,19 +1,25 @@
 // This function should return true if num is greater
 // than or equal to lower AND less than or equal to upper.
 // Implement this with a single condition.
-function isInRange (num, lower, upper) {
-
+function isInRange(num, lower, upper) {
   // TODO: write code in this function body to pass the tests
-
+  if (num >= lower && num <= upper) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // This function should return true if the passed string is equal
 // to "Hello" or "Goodbye". Implement this with a single
 // if statement.
-function isHelloOrGoodbye (val1) {
-
+function isHelloOrGoodbye(val1) {
   // TODO: write code in this function body to pass the tests
-
+  if (val1 === 'Hello' || val1 === 'Goodbye') {
+    return true
+  } else {
+    return false
+  }
 }
 
 // This function should return a string that describes the provided age value. The
@@ -28,9 +34,22 @@ function isHelloOrGoodbye (val1) {
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
-function getAgeDescription (age) {
-
+function getAgeDescription(age) {
   // TODO: write code in this function body to pass the tests
+  ageCategory = ''
+
+  if (age === 0) {
+    ageCategory = 'Baby'
+  } else if (age >= 1 && age <= 4) {
+    ageCategory = 'Toddler'
+  } else if (age >= 5 && age <= 12) {
+    ageCategory = 'Child'
+  } else if (age >= 13 && age <= 19) {
+    ageCategory = 'Teenager'
+  } else {
+    ageCategory = 'Adult'
+  }
+  return ageCategory
 }
 
 module.exports = {
